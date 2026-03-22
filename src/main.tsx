cf6df3413c8d34dad04963f669e7c0d8db2e13f0
fabrisdev <ezepuntocom@hotmail.com>
2026-03-22 16:51:39 -0300

    remove default styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 
 const rootDiv = document.getElementById("root");
 if (rootDiv === null) throw new Error("Missing root div in HTML");
